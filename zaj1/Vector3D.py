f0f2fdafa1d99bd49c4d4818a778a2e16703408a
Bartłomiej <bartlomiej.runowski@gmail.com>
2024-03-02 09:20:10 +0100

    Add Vector class implementation

---
+++ zaj1/Vector3D.py
@@ -0,0 +1,36 @@
+import math
+
+
+class Vector3D:
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def __str__(self):
+        return f"Vector3D({self.x}, {self.y}, {self.z})"
+
+    def norm(self):
+        return math.sqrt(self.x**2 + self.y**2 + self.z**2)
+
+    def __add__(self, other):
+        return Vector3D(self.x + other.x, self.y + other.y, self.z + other.z)
+
+    def __sub__(self, other):
+        return Vector3D(self.x - other.x, self.y - other.y, self.z - other.z)
+
+    def __mul__(self, scalar):
+        return Vector3D(self.x * scalar, self.y * scalar, self.z * scalar)
+
+    def dot(self, other):
+        return self.x * other.x, self.y * other.y, self.z * other.z
+
+    def cross(self, other):
+        cross_x = self.y * other.z - self.z * other.y
+        cross_y = self.z * other.x - self.x * other.z
+        cross_z = self.x * other.y - self.y * other.x
+        return Vector3D(cross_x, cross_y, cross_z)
+
+    @staticmethod
+    def are_orthogonal(v1, v2):
+        return v1.dot(v2) == 0
